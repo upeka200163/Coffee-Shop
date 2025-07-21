442b1ff2bf4836aab917c064daa040b8fa51b7cf
Upeka Sewwandi <upekasewwandi81@gmail.com>
2025-07-21 21:21:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+
+let swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+    
+      autoplay:{
+      delay: 3000,
+      disableOnInteraction: false
+    },
+
+  
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
